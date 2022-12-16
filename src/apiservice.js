83562d8f4c51a8ffd15e49bd23adc511721f81cd
Владислав Чернышев <vladdizlaw@gmail.com>
2022-12-17 02:13:21 +0300

    added headers in axios

--- src/apiservice.js
+++ src/apiservice.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded'
+axios.defaults.headers.get['Content-Type'] ='application/x-www-form-urlencoded'
 class ApiService {
   async getRubrics() {
     try {
