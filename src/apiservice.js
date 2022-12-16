5cec70272ac3f3b1a3f8a668f0f5d892d1cf46d5
Владислав Чернышев <vladdizlaw@gmail.com>
2022-12-17 02:29:58 +0300

    added headers in axios

--- src/apiservice.js
+++ src/apiservice.js
@@ -1,12 +1,26 @@
 import axios from "axios";
-axios.defaults.headers.get['Content-Type'] ='application/x-www-form-urlencoded'
+
+// axios.defaults.headers.get['Content-Type'] ='application/x-www-form-urlencoded'
+// axios.defaults.headers.get['Access-Control-Allow-Headers']='Content-Type, X-Auth-Token, Origin'
+// axios.defaults.headers.get['Access-Control-Allow-Methods']='POST, GET, OPTIONS, PUT, DELETE'
 class ApiService {
   async getRubrics() {
     try {
-      const rubrics = await axios.get(
-        "https://www.klerk.ru/yindex.php/v3/event/rubrics"
+      const rubrics = await fetch(
+        "https://www.klerk.ru/yindex.php/v3/event/rubrics",
+        {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          }
+        }
       );
-      return rubrics.data;
+      console.log(rubrics)
+      return rubrics.json();
     } catch (err) {
       console.log(err);
     }
