076f51b9ab7fa273c4cf808e085c4cc1c93ccdd6
Владислав Чернышев <vladdizlaw@gmail.com>
2022-12-18 02:27:59 +0300

    added netlify

--- Vue.config.js
+++ Vue.config.js
@@ -1,9 +1,9 @@
-devServer: {
-    proxy: {
-      "/api": {
-        ws: true,
-        changeOrigin: true,
-        target: "https://www.klerk.ru/yindex.php/v3/event/rubrics"
-      }
-    }
-  }
+// devServer: {
+//     proxy: {
+//       "/api": {
+//         ws: true,
+//         changeOrigin: true,
+//         target: "https://www.klerk.ru/yindex.php/v3/event/rubrics"
+//       }
+//     }
+//   }
