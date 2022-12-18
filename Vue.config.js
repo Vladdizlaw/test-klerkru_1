51beca3e6a173c12f493f07cadaff252d1eed9c0
Владислав Чернышев <vladdizlaw@gmail.com>
2022-12-18 10:35:58 +0300

    cors

--- Vue.config.js
+++
@@ -1,9 +0,0 @@
-// devServer: {
-//     proxy: {
-//       "/api": {
-//         ws: true,
-//         changeOrigin: true,
-//         target: "https://www.klerk.ru/yindex.php/v3/event/rubrics"
-//       }
-//     }
-//   }
